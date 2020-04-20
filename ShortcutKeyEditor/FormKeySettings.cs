ce3d8767e037e7ed08173f25a5b05ed21f5f7825
wertrain <wertrain@gmail.com>
2020-04-20 23:51:43 +0900

    ローカライズ処理の修正

--- ShortcutKeyEditor/FormKeySettings.cs
+++ ShortcutKeyEditor/FormKeySettings.cs
@@ -24,20 +24,6 @@ namespace ShortcutKeyEditor
         void Localize()
         {
             LocalizeUtil.Localized(this);
-            LocalizeUtil.Localized(groupBoxCommands);
-            LocalizeUtil.Localized(groupBoxCommandDescription);
-            LocalizeUtil.Localized(columnHeaderCommandName);
-            LocalizeUtil.Localized(columnHeaderCommandKeys);
-            LocalizeUtil.Localized(groupBoxCurrentShortcut);
-            LocalizeUtil.Localized(buttonSetToDefault);
-            LocalizeUtil.Localized(buttonRemoveShortcut);
-            LocalizeUtil.Localized(groupBoxNewShortcut);
-            LocalizeUtil.Localized(buttonAddShortcut);
-            LocalizeUtil.Localized(buttonAssignShortcut);
-            LocalizeUtil.Localized(groupBoxShortcutUsed);
-            LocalizeUtil.Localized(buttonSetAllToDefault);
-            LocalizeUtil.Localized(buttonOk);
-            LocalizeUtil.Localized(buttonCancel);
         }
     }
 }
--- ShortcutKeyEditor/Localize/LocalizeUtil.cs
+++ ShortcutKeyEditor/Localize/LocalizeUtil.cs
@@ -8,9 +8,26 @@ namespace ShortcutKeyEditor.Localize
         /// 
         /// </summary>
         /// <param name="control"></param>
-        public static void Localized(Control control)
+        public static void Localized (Control control)
         {
             control.Text = control.Text.Localize();
+            foreach (var child in control.Controls)
+            {
+                if (child is Control)
+                {
+                    var childControl = child as Control;
+                    Localized(childControl);
+                }
+
+                if (child is ListView)
+                {
+                    var childListView = child as ListView;
+                    foreach (ColumnHeader column in childListView.Columns)
+                    {
+                        column.Text = column.Text.Localize();
+                    }
+                }
+            }
         }
 
         /// <summary>
